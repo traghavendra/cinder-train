fe2ea5b25bee2f00ad7d1a224c19e0daf725cbbe
raghavendrat <raghavendra.tilay@hpe.com>
2022-01-18 12:00:01 +0000

    Update UT for multi-detach and Primera iscsi support

--- cinder/tests/unit/volume/drivers/hpe/test_hpe3par.py
+++ cinder/tests/unit/volume/drivers/hpe/test_hpe3par.py
@@ -763,14 +763,15 @@ class HPE3PARBaseDriver(test.TestCase):
         spec=True,
     )
     def setup_mock_client(self, _m_client, driver, conf=None, m_conf=None,
-                          is_primera=False):
+                          is_primera=False,
+                          wsapi_version=wsapi_version_latest):
 
         _m_client = _m_client.return_value
 
         # Configure the base constants, defaults etc...
         _m_client.configure_mock(**self.mock_client_conf)
 
-        _m_client.getWsApiVersion.return_value = self.wsapi_version_latest
+        _m_client.getWsApiVersion.return_value = wsapi_version
 
         _m_client.is_primera_array.return_value = is_primera
 
@@ -8912,10 +8913,49 @@ class TestHPE3PARISCSIDriver(HPE3PARBaseDriver):
 
         return mock_client
 
-    def test_iscsi_primera(self):
+    def test_iscsi_primera_old(self):
+        # primera 4.0.xx.yyy
+        wsapi_version_primera_old = {'major': 1,
+                                     'build': 40000128,
+                                     'minor': 8,
+                                     'revision': 1}
+
         self.assertRaises(NotImplementedError, self.setup_mock_client,
                           driver=hpedriver.HPE3PARISCSIDriver,
-                          is_primera=True)
+                          is_primera=True,
+                          wsapi_version=wsapi_version_primera_old)
+
+    def test_iscsi_primera_new(self, config=None, mock_conf=None):
+        # primera 4.2.xx.yyy
+        wsapi_version_primera_new = {'major': 1,
+                                     'build': 40202010,
+                                     'minor': 8,
+                                     'revision': 1}
+
+        self.ctxt = context.get_admin_context()
+
+        mock_client = self.setup_mock_client(
+            conf=config,
+            m_conf=mock_conf,
+            driver=hpedriver.HPE3PARISCSIDriver,
+            is_primera=True,
+            wsapi_version=wsapi_version_primera_new)
+
+        expected_get_cpgs = [
+            mock.call.getCPG(HPE3PAR_CPG),
+            mock.call.getCPG(HPE3PAR_CPG2)]
+        expected_get_ports = [mock.call.getPorts()]
+        expected_primera = [
+            mock.call.is_primera_array(),
+            mock.call.getWsApiVersion()]
+        mock_client.assert_has_calls(
+            self.standard_login +
+            expected_get_cpgs +
+            self.standard_logout +
+            expected_primera +
+            self.standard_login +
+            expected_get_ports +
+            self.standard_logout)
 
     @ddt.data('volume', 'volume_name_id')
     def test_initialize_connection(self, volume_attr):
@@ -10930,13 +10970,13 @@ class TestHPE3PARISCSIDriver(HPE3PARBaseDriver):
     def test_migrate_volume_attached(self):
         self.migrate_volume_attached()
 
-    def test_terminate_connection_multiattach(self):
+    def test_terminate_connection_multiattach_same_host(self):
         ctx = context.get_admin_context()
         mock_client = self.setup_driver()
         att_1 = fake_volume.volume_attachment_ovo(
-            ctx, id=uuidutils.generate_uuid())
+            ctx, id=uuidutils.generate_uuid(), attached_host='same_host')
         att_2 = fake_volume.volume_attachment_ovo(
-            ctx, id=uuidutils.generate_uuid())
+            ctx, id=uuidutils.generate_uuid(), attached_host='same_host')
         volume = fake_volume.fake_volume_obj(
             ctx, multiattach=True, host=self.FAKE_CINDER_HOST)
         volume.volume_attachment.objects = [att_1, att_2]
@@ -10945,13 +10985,74 @@ class TestHPE3PARISCSIDriver(HPE3PARBaseDriver):
             mock_create_client.return_value = mock_client
             self.driver.terminate_connection(volume, self.connector)
 
-            # When volume is having mulitple instances attached, there
+            # When volume is attached to mulitple instances on same host, there
             # should be no call to delete the VLUN(s) or the host. We
             # can assert these methods were not called to make sure the
             # proper code execution is followed.
 
             self.assertEqual(0, mock_client.deleteVLUN.call_count)
             self.assertEqual(0, mock_client.deleteHost.call_count)
+
+    def test_terminate_connection_multiattach_different_host(self):
+        ctx = context.get_admin_context()
+        att_1 = fake_volume.volume_attachment_ovo(
+            ctx, id=uuidutils.generate_uuid(), attached_host='host_one')
+        att_2 = fake_volume.volume_attachment_ovo(
+            ctx, id=uuidutils.generate_uuid(), attached_host='host_two')
+        volume = fake_volume.fake_volume_obj(
+            ctx, multiattach=True, host=self.FAKE_CINDER_HOST)
+        volume.volume_attachment.objects = [att_1, att_2]
+
+        vol_name = 'osv-HlF355XlSg.xcORfS0afag'
+
+        # When volume is attached to instances on different hosts,
+        # VLUN(s) of that host should be deleted. We can assert
+        # appropriate methods were called.
+
+        mock_client = self.setup_driver()
+        mock_client.getStorageSystemInfo.return_value = (
+            {'id': self.CLIENT_ID})
+
+        mock_client.getHostVLUNs.return_value = [
+            {'active': False,
+             'volumeName': vol_name,
+             'lun': None, 'type': 0}]
+
+        mock_client.queryHost.return_value = {
+            'members': [{
+                'name': self.FAKE_HOST
+            }]
+        }
+
+        with mock.patch.object(hpecommon.HPE3PARCommon,
+                               '_create_client') as mock_create_client:
+            mock_create_client.return_value = mock_client
+            self.driver.terminate_connection(
+                volume,
+                self.connector,
+                force=True)
+
+            expected = [
+                mock.call.queryHost(iqns=[self.connector['initiator']]),
+                mock.call.getHostVLUNs(self.FAKE_HOST),
+                mock.call.deleteVLUN(
+                    vol_name,
+                    None,
+                    hostname=self.FAKE_HOST),
+                mock.call.getHostVLUNs(self.FAKE_HOST),
+                mock.call.modifyHost(
+                    'fakehost',
+                    {'pathOperation': 2,
+                     'iSCSINames': ['iqn.1993-08.org.debian:01:222']}),
+                mock.call.removeVolumeMetaData(vol_name, CHAP_USER_KEY),
+                mock.call.removeVolumeMetaData(vol_name, CHAP_PASS_KEY)]
+
+            mock_client.assert_has_calls(
+                self.get_id_login +
+                self.standard_logout +
+                self.standard_login +
+                expected +
+                self.standard_logout)
 
     @ddt.data('volume', 'volume_name_id')
     def test_terminate_connection(self, volume_attr):
